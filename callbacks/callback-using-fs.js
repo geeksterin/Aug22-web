e644a55911a152903643f4a62fce86c1c42ec1d7
bhavesh-appbrew <bhavesh@appbrew.tech>
2022-10-25 20:46:38 +0530

    callback notes

--- callbacks/callback-using-fs.js
+++ callbacks/callback-using-fs.js
@@ -20,16 +20,43 @@ const fs = require('fs')
 
 // write 8 files synchrounously with random numbers
 
-for(let i= 1; i<= 8; i++) {
-    fs.writeFileSync(`test${i}.txt`,JSON.stringify(Math.round(Math.random()*100)))
-    console.log(`file ${i} wrote successfully.`)
-    const data = fs.readFileSync(`test${i}.txt`,'utf-8')
-    console.log(data)
-}
-
 // for(let i= 1; i<= 8; i++) {
-//     fs.writeFile(`test${i}.txt`,JSON.stringify(Math.round(Math.random()*100)), () => {
-//         console.log(`file ${i} wrote successfully.`)
-//     })
-    
+//     fs.writeFileSync(`test${i}.txt`,JSON.stringify(Math.round(Math.random()*100)))
+//     console.log(`file ${i} wrote successfully.`)
+//     const data = fs.readFileSync(`test${i}.txt`,'utf-8')
+//     console.log(data)
 // }
+
+for (let i = 1; i <= 8; i++) {
+    fs.writeFile(`test${i}.txt`, JSON.stringify(Math.round(Math.random() * 100)), () => {
+        console.log(`file ${i} wrote successfully.`)
+        fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+            console.log(`file ${i} read successfully.`)
+            console.log(data)
+            fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                console.log(`file ${i} read successfully.`)
+                console.log(data)
+                fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                    console.log(`file ${i} read successfully.`)
+                    console.log(data)
+                    fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                        console.log(`file ${i} read successfully.`)
+                        console.log(data)
+                        fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                            console.log(`file ${i} read successfully.`)
+                            console.log(data)
+                            fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                                console.log(`file ${i} read successfully.`)
+                                console.log(data)
+                                fs.readFile(`test${i}.txt`, 'utf-8', (err, data) => {
+                                    console.log(`file ${i} read successfully.`)
+                                    console.log(data)
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+}
