55106ec439299a5f1fec93fb1a4bb56a8d366352
bhavesh-appbrew <bhavesh@appbrew.tech>
2022-12-23 21:26:51 +0530

    crypto view part -2

---
+++ milestone/script.js
@@ -0,0 +1,59 @@
+const trendingCoinsSlideshow = document.getElementById('trending_coins_slideshow')
+
+init()
+
+function init() {
+    getTrendingCoins()
+}
+
+function scrollAnimation() {
+    let count = 0
+    let flag = true // true means going up else down
+    setInterval(() => {
+        //     const paddingLeft = Number(getComputedStyle(trendingCoinsSlideshow).paddingLeft.replace("px", ""))
+        // const paddingRight = Number(getComputedStyle(trendingCoinsSlideshow).paddingLeft.replace("px", ""))
+        const endPoint = trendingCoinsSlideshow.scrollWidth - trendingCoinsSlideshow.offsetWidth
+        if (flag) {
+            count += 1
+            if (count === endPoint) {
+                flag = false
+            }
+        } else {
+            count -= 1
+            if (count === 0) {
+                flag = true
+            }
+        }
+        trendingCoinsSlideshow.scrollTo(count, 0)
+    }, 10)
+
+}
+
+async function getTrendingCoins() {
+    const res = await fetch('https://api.coingecko.com/api/v3/search/trending')
+    const jsonData = await res.json()
+    const bitcoinVaue = await getBitcoinValue()
+    const coins = jsonData.coins
+    let html = ''
+    for (let i = 0; i < coins.length; i++) {
+        const coinName = coins[i].item.name
+        const coinPrice = coins[i].item.price_btc * bitcoinVaue
+        const coinSymbol = coins[i].item.symbol
+        const coinIconUrl = coins[i].item.small
+        html += `<div class="trending_coin_container">
+        <img src="${coinIconUrl}" class="coin_logo" alt="coin logo" />
+        <div class="coin_info_container">
+          <h1 class="coin_name">${coinName} (${coinSymbol})</h1>
+          <h4 class="coin_price">Rs ${coinPrice.toFixed(4)}</h4>
+        </div>
+      </div>`
+    }
+    trendingCoinsSlideshow.innerHTML = html
+    scrollAnimation()
+}
+
+async function getBitcoinValue() {
+    const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr')
+    const jsonData = await res.json()
+    return jsonData.bitcoin.inr
+}
